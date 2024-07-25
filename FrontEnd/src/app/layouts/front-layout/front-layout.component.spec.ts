f55763377d8b0d4ce14706667f46eb7edfac90c1
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-25 01:28:28 +0100

    Create front-layout.component.spec.ts

---
+++ FrontEnd/src/app/layouts/front-layout/front-layout.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FrontLayoutComponent } from './front-layout.component';
+
+describe('FrontLayoutComponent', () => {
+  let component: FrontLayoutComponent;
+  let fixture: ComponentFixture<FrontLayoutComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FrontLayoutComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FrontLayoutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
