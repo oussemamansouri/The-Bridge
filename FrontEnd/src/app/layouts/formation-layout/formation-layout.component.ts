f8e011e3c55aaf83f646dc9b1a31372abf9da8ae
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-25 01:28:22 +0100

    Create formation-layout.component.ts

---
+++ FrontEnd/src/app/layouts/formation-layout/formation-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-formation-layout',
+  templateUrl: './formation-layout.component.html',
+  styleUrls: ['./formation-layout.component.scss']
+})
+export class FormationLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
