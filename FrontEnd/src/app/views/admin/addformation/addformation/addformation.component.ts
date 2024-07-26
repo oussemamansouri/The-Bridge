2cb2eb4ec2471f2e9eb34069c7c379be5a7c4b72
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-26 15:09:51 +0100

    Create addformation.component.ts

---
+++ FrontEnd/src/app/views/admin/addformation/addformation/addformation.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addformation',
+  templateUrl: './addformation.component.html',
+  styleUrls: ['./addformation.component.scss']
+})
+export class AddformationComponent implements OnInit {
+
+
+
+  
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
