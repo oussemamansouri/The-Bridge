fb4d0f4941cf3f0a75db641e92fed0c9187e17e2
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-26 15:23:24 +0100

    Create consultformation.component.ts

---
+++ FrontEnd/src/app/views/moderator/consultformation/consultformation/consultformation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consultformation',
+  templateUrl: './consultformation.component.html',
+  styleUrls: ['./consultformation.component.scss']
+})
+export class ConsultformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
