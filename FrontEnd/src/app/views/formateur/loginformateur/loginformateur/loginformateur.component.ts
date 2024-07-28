fff74d673f18fa3fb36181ad0aae4c997b351666
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-28 19:22:16 +0100

    Create loginformateur.component.ts

---
+++ FrontEnd/src/app/views/formateur/loginformateur/loginformateur/loginformateur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginformateur',
+  templateUrl: './loginformateur.component.html',
+  styleUrls: ['./loginformateur.component.scss']
+})
+export class LoginformateurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
