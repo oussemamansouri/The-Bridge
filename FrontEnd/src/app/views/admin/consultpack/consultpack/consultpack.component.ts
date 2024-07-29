1f8aa6a624a2df7dba1dd5f12ab691c8b43c1259
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-29 14:58:45 +0100

    Create consultpack.component.ts

---
+++ FrontEnd/src/app/views/admin/consultpack/consultpack/consultpack.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-consultpack',
+  templateUrl: './consultpack.component.html',
+  styleUrls: ['./consultpack.component.scss']
+})
+export class ConsultpackComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
