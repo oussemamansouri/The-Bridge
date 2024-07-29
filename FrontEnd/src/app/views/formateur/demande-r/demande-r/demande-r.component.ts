d567136b9f697ed8d9a516f576866cf73c3f5d24
oussemamansouri <oussemamansouri619@gmail.com>
2024-07-30 00:24:30 +0100

    Create demande-r.component.ts

---
+++ FrontEnd/src/app/views/formateur/demande-r/demande-r/demande-r.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demande-r',
+  templateUrl: './demande-r.component.html',
+  styleUrls: ['./demande-r.component.scss']
+})
+export class DemandeRComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
