5c1b81af618bb4a7f9f561b9bf536404b578087f
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-01 03:15:58 +0100

    Create guardadmin.guard.ts

---
+++ FrontEnd/src/app/views/guards/guardadmin.guard.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthadminService } from '../services/authadmin.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GuardadminGuard implements CanActivate {
+  constructor(private as:AuthadminService,private router:Router){
+
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return new Promise((resolve,reject)=>{
+      if(this.as.LoggedIn()==true){
+        resolve(true)
+      }
+      else {
+        this.router.navigate(['/loginuser'])
+        localStorage.removeItem('token')
+        resolve(false)
+      }
+    })
+  }
+  
+}
