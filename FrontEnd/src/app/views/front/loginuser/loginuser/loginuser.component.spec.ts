7dcda446c37284dbbac10ed92052e9dbd7b71cbe
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-01 03:16:23 +0100

    Create loginuser.component.spec.ts

---
+++ FrontEnd/src/app/views/front/loginuser/loginuser/loginuser.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LoginuserComponent } from './loginuser.component';
+
+describe('LoginuserComponent', () => {
+  let component: LoginuserComponent;
+  let fixture: ComponentFixture<LoginuserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LoginuserComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LoginuserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
