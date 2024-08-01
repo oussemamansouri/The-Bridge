5b919a9f97af59ad3735422fc4e8b033ec4e2ccf
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-01 03:16:03 +0100

    Create tarifs.component.ts

---
+++ FrontEnd/src/app/views/front/tarifs/tarifs/tarifs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tarifs',
+  templateUrl: './tarifs.component.html',
+  styleUrls: ['./tarifs.component.scss']
+})
+export class TarifsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
