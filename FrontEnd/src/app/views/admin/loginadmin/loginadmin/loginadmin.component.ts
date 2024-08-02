a2e1d38ac5f1637d32829d131ccfbd3f54d1a314
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-02 02:52:57 +0100

    Create loginadmin.component.ts

---
+++ FrontEnd/src/app/views/admin/loginadmin/loginadmin/loginadmin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-loginadmin',
+  templateUrl: './loginadmin.component.html',
+  styleUrls: ['./loginadmin.component.scss']
+})
+export class LoginadminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
