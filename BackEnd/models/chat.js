99902fc3c4ad0f8156589b8fc36f46993d1d180b
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-03 00:56:30 +0100

    Update chat.js

--- BackEnd/models/chat.js
+++ BackEnd/models/chat.js
@@ -1,38 +1,32 @@
 module.exports = (sequelize, DataTypes) => {
-    const ChatMessage = sequelize.define("ChatMessage", {
-      id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
-      },
-      message: {
-        type: DataTypes.TEXT,
-        allowNull: false
-      },
-      timestamp: {
-        type: DataTypes.DATE,
-        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
-      }
-    }, {
-      charset: 'utf8mb4',
-      collate: 'utf8mb4_general_ci'
+  const ChatMessage = sequelize.define('ChatMessage', {
+    message: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    sender_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    recipient_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    }
+  });
+
+  ChatMessage.associate = models => {
+    ChatMessage.belongsTo(models.Formateur, {
+      foreignKey: 'sender_id',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
     });
-  
-    ChatMessage.associate = (models) => {
-      // Association avec le formateur expéditeur
-      ChatMessage.belongsTo(models.Formateur, {
-        foreignKey: 'sender_id',
-        as: 'sender',
-        allowNull: false
-      });
-      
-      // Association avec le formateur destinataire (facultatif)
-      ChatMessage.belongsTo(models.Formateur, {
-        foreignKey: 'recipient_id',
-        as: 'recipient'
-      });
-    };
-  
-    return ChatMessage;
+
+    ChatMessage.belongsTo(models.Formateur, {
+      foreignKey: 'recipient_id',
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    });
   };
-  
+
+  return ChatMessage;
+};
