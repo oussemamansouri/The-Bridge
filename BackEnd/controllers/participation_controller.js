ee92d8d197b31668864bf41345f1f22986a95617
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-03 00:56:26 +0100

    Update participation_controller.js

--- BackEnd/controllers/participation_controller.js
+++ BackEnd/controllers/participation_controller.js
@@ -7,42 +7,40 @@ const SchemaValidation=Joi.object({
     FormateurId :Joi.number().integer().positive().required(),
     FormationId :Joi.number().integer().positive().required(),
     ReceiverId :Joi.number().integer().positive().required(),
-
-  
 })
 
-
-const accepte=(FormateurId,FormationId,ReceiverId)=>{
-    return new Promise((resolve,reject)=>{
-        let validation=SchemaValidation.validate({FormateurId,FormationId,ReceiverId})
-        if (validation.error){
-            reject(validation.error.details[0].message)
-        }else{
-        db.Participation.count({where:{FormateurId:FormateurId,FormationId:FormationId}}).then(doc=>{
-            if (doc!=0){
-                reject("Le formateur participe déjà à cette formation")
-            }else{
-
-                db.Demande.destroy({where:{ReceiverId:ReceiverId,FormateurId:FormateurId,FormationId:FormationId}})
-                db.Participation.create({
-                    FormateurId:FormateurId,
-                    FormationId:FormationId,
-                    ReceiverId:ReceiverId
-                }).then((response)=>resolve(response))
-                .catch((err)=>reject(err))
-                
-              
-            }
-        })}
-    })
-}
+const accepte = (FormateurId, FormationId, ReceiverId) => {
+    return new Promise((resolve, reject) => {
+        let validation = SchemaValidation.validate({ FormateurId, FormationId, ReceiverId });
+        if (validation.error) {
+            reject(validation.error.details[0].message);
+        } else {
+            db.Participation.count({ where: { FormateurId: FormateurId, FormationId: FormationId } }).then(doc => {
+                if (doc != 0) {
+                    reject("Le formateur participe déjà à cette formation");
+                } else {
+                    db.Demande.destroy({ where: { ReceiverId: ReceiverId, FormateurId: FormateurId, FormationId: FormationId } })
+                        .then(() => {
+                            return db.Participation.bulkCreate([
+                                { FormateurId: FormateurId, FormationId: FormationId, ReceiverId: ReceiverId },
+                                { FormateurId: ReceiverId, FormationId: FormationId, ReceiverId: FormateurId }
+                            ]);
+                        })
+                        .then((response) => resolve(response))
+                        .catch((err) => reject(err));
+                }
+            }).catch((err) => reject(err));
+        }
+    });
+};
 
 const Deleteparticipation = (req, res) => {
-    const clientid = req.params.clientid;
-    const formationid = req.params.formationid;
+    const FormateurId = req.params.FormateurId;
+    const FormationId = req.params.FormationId;
+    const ReceiverId = req.params.ReceiverId;
 
     db.Participation.destroy({
-      where: { ClientId: clientid,FormationId: formationid}
+      where: { FormateurId: FormateurId,FormationId: FormationId,ReceiverId:ReceiverId}
     })
       .then(num => {
         if (num == 1) {
@@ -51,7 +49,7 @@ const Deleteparticipation = (req, res) => {
           });
         } else {
           res.send({
-            message: `Cannot delete participation with clientid=${clientid} and formationid=${formationid}. Maybe participation was not found!`
+            message: `Cannot delete participation with Formateur=${FormateurId} and formationid=${FormationId}. Maybe participation was not found!`
           });
         }
       })
@@ -61,9 +59,29 @@ const Deleteparticipation = (req, res) => {
         });
       });
   }
+  const getAcceptedFriends = async (FormateurId) => {
+    try {
+        const participations = await db.Participation.findAll({
+            where: { FormateurId: FormateurId },
+            include: [
+                {
+                    model: db.Formateur,
+                    as: 'Friend'
+                }
+            ],
+            group: ['ReceiverId']
+        });
 
+        const uniqueFriends = participations.map(participation => participation.Friend);
+
+        return uniqueFriends;
+    } catch (err) {
+        throw new Error(err);
+    }
+};
 
 module.exports = {
     accepte,
-    Deleteparticipation
+    Deleteparticipation,
+    getAcceptedFriends
 }
