fd0fa9b902eecc442d0178c210a74ea632a6b272
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-03 00:57:06 +0100

    Update admin_router.js

--- BackEnd/routers/admin_router.js
+++ BackEnd/routers/admin_router.js
@@ -7,9 +7,6 @@ const admin_controller=require('../controllers/admin_controller')
 const path=require('path')
 
 
-
-
-
 //////////////////////////////  register api start here ///////////////////////////////////////////////:
 
 route.post('/register', admin_controller.upload, (req, res) => {
@@ -21,10 +18,6 @@ route.post('/register', admin_controller.upload, (req, res) => {
     .catch(err=>res.status(400).json(err))     
     
 })
-
-//////////////////////////////////   register api start here     //////////////////////////////////////////
-
-
 
 //////////////////////////////////   update password api start here     //////////////////////////////////////////
 
@@ -40,7 +33,6 @@ route.patch('/updatepassword/:id', (req, res) => {
   })
 //////////////////////////////////   update password api end here     //////////////////////////////////////////
 
-
     route.patch('/updateprofile/:id',(req,res)=>{
         admin_controller.updateprofile(req.body.username,req.body.email,req.body.tel,req.params.id)
         .then(response=>res.status(200).json(response))
@@ -54,11 +46,6 @@ route.patch('/updatepassword/:id', (req, res) => {
           .catch(err => res.status(400).json(err))
       })
 
-
-
-
-
-
     route.get('/profile',(req,res,next)=>{ 
 
         db.Admin.findOne()
@@ -66,50 +53,6 @@ route.patch('/updatepassword/:id', (req, res) => {
         .catch((err)=>res.status(400).json(err))
         
         })
-
-
-            
-//////////////////////////////   verify token client
-
-// verifytokenadmin=(req,res,next)=>{
-
-//     let token=req.headers.authorization
-//     let role=req.headers.role
-//     if(!token || role!='admin'){
-//         res.status(400).json({msg:'access rejected....!!!!'})
-//     }
-    
-//     try{
-//         jwt.verify(token,process.env.PRIVATKEY)
-//         next()
-//     }catch(e){
-//     res.status(400).json({msg:e})
-//     }
-    
-//     }
-
-
-/////////////////////////////////////// verify keys
-
-// var key1=process.env.KEY1
-// var key2=process.env.KEY2
-
-// verifykey=(req,res,next)=>{
-
-//     let pk=req.query.key1
-//     let ck=req.query.key2
-
-// if(pk==key1 && ck==key2 ){
-//     next()
-// }else{
-//     res.status(400).json({error:"you can't access this route because you don't send me the keys i need !!!"})
-// }
-
-// }
-
-
-
-            
 
 
 module.exports = route
