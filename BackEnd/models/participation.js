944f3a8da67acb1a7bf309ce763248308e36e543
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-03 00:56:47 +0100

    Update participation.js

--- BackEnd/models/participation.js
+++ BackEnd/models/participation.js
@@ -1,27 +1,33 @@
-module.exports=(sequelize,DataType)=>{
-    const Participation=sequelize.define("Participation",{
-      ReceiverId:{
-        type:DataType.BIGINT,
-        allowNull:true
-    } 
-    },{
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_general_ci'
-      })
+module.exports = (sequelize, DataType) => {
+  const Participation = sequelize.define("Participation", {
+      ReceiverId: {
+          type: DataType.BIGINT,
+          allowNull: true
+      }
+  }
+  , {
+      charset: 'utf8mb4',
+      collate: 'utf8mb4_general_ci'
+  });
 
-    Participation.associate=models=>{
+  Participation.associate = models => {
+      Participation.belongsTo(models.Formateur, {
+          as: 'Formateur',
+          foreignKey: 'FormateurId',
+          onDelete: "cascade",
+          onUpdate: 'cascade'
+      });
+      Participation.belongsTo(models.Formateur, {
+          as: 'Friend',
+          foreignKey: 'ReceiverId',
+          onDelete: "cascade",
+          onUpdate: 'cascade'
+      });
+      Participation.belongsTo(models.Formation, {
+          onDelete: "cascade",
+          onUpdate: 'cascade'
+      });
+  };
 
-        Participation.belongsTo(models.Formateur, {
-            onDelete: "cascade",
-            onUpdate: 'cascade'
-         })
-         Participation.belongsTo(models.Formation, {
-            onDelete: "cascade",
-            onUpdate: 'cascade'
-         })
-
-    }  
-
-return Participation
-
-} 
+  return Participation;
+};
