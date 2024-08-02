c8692bc63f46fc6cf5cfd1b855936b30069119ee
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-03 00:57:47 +0100

    Update moderateur_router.js

--- BackEnd/routers/moderateur_router.js
+++ BackEnd/routers/moderateur_router.js
@@ -53,8 +53,6 @@ route.patch('/updateprofile/:id',(req,res)=>{
     .catch(err=>res.status(400).json(err))
 })
 
-//////////////////////////////////   register api start here     //////////////////////////////////////////
-
 
 
 //////////////////////////////////   update password api start here     //////////////////////////////////////////
@@ -79,55 +77,5 @@ route.patch('/updatepassword/:id', (req, res) => {
           .then(response => res.status(200).json(response))
           .catch(err => res.status(400).json(err))
       })
-
-
-
-   
-
-
-
-            
-//////////////////////////////   verify token client
-
-// verifytokenmoderateur=(req,res,next)=>{
-
-//     let token=req.headers.authorization
-//     let role=req.headers.role
-//     if(!token || role!='moderateur'){
-//         res.status(400).json({msg:'access rejected....!!!!'})
-//     }
-    
-//     try{
-//         jwt.verify(token,process.env.PRIVATKEY)
-//         next()
-//     }catch(e){
-//     res.status(400).json({msg:e})
-//     }
-    
-//     }
-
-
-// /////////////////////////////////////// verify keys
-
-// var key1=process.env.KEY1
-// var key2=process.env.KEY2
-
-// verifykey=(req,res,next)=>{
-
-//     let pk=req.query.key1
-//     let ck=req.query.key2
-
-// if(pk==key1 && ck==key2 ){
-//     next()
-// }else{
-//     res.status(400).json({error:"you can't access this route because you don't send me the keys i need !!!"})
-// }
-
-// }
-
-
-
-            
-
 
 module.exports=route
