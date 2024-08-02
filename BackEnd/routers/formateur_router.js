cbf57d85eb8e59a617d124d0895522dcb43241bc
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-03 00:57:26 +0100

    Update formateur_router.js

--- BackEnd/routers/formateur_router.js
+++ BackEnd/routers/formateur_router.js
@@ -33,7 +33,6 @@ formateur_controller.register(req.body.firstname,req.body.lastname,req.body.emai
 
 })
 
-//////////////////////////////////   register api start here     //////////////////////////////////////////
 
 
 
@@ -49,14 +48,6 @@ route.patch('/updatepassword/:id', (req, res) => {
       .then(response => res.status(200).json(response))
       .catch(err => res.status(400).json(err))
   })
-
-
-
-
-
-
-
-
 
 //////////////////////////////////   update password api end here     //////////////////////////////////////////
 
@@ -112,56 +103,5 @@ route.patch('/updatepassword/:id', (req, res) => {
               .catch((err)=>res.status(400).json(err))
               
               })
-
-
-
-
-
-
-
-
-            
-//////////////////////////////   verify token client
-
-// verifytokenclient=(req,res,next)=>{
-
-//     let token=req.headers.authorization
-//     let role=req.headers.role
-//     if(!token || role!='client'){
-//         res.status(400).json({msg:'access rejected....!!!!'})
-//     }
-    
-//     try{
-//         jwt.verify(token,process.env.PRIVATKEY)
-//         next()
-//     }catch(e){
-//     res.status(400).json({msg:e})
-//     }
-    
-//     }
-
-
-// /////////////////////////////////////// verify keys
-
-// var key1=process.env.KEY1
-// var key2=process.env.KEY2
-
-// verifykey=(req,res,next)=>{
-
-//     let pk=req.query.key1
-//     let ck=req.query.key2
-
-// if(pk==key1 && ck==key2 ){
-//     next()
-// }else{
-//     res.status(400).json({error:"you can't access this route because you don't send me the keys i need !!!"})
-// }
-
-// }
-
-
-
-            
-
 
 module.exports=route
