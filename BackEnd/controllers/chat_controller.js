a30bc041f2483bbec52eaa3bfeb1c5546eb2b881
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-03 00:54:41 +0100

    Update chat_controller.js

--- BackEnd/controllers/chat_controller.js
+++ BackEnd/controllers/chat_controller.js
@@ -1,34 +1,21 @@
-const { ChatMessage } = require('../models');
+const db = require('../models');
 
-// Contrôleur pour créer un nouveau message
-exports.createMessage = async (req, res) => {
+
+
+exports.getAllMessages = async (recipientId) => {
     try {
-      const { message, sender_id, recipient_id, formateur_id } = req.body; // Ajouter formateur_id
-      const newMessage = await ChatMessage.create({ message, sender_id, recipient_id, formateur_id });
-      
-      // Ici, vous pouvez également utiliser le gestionnaire de sockets pour diffuser le nouveau message
-      
-      res.status(201).json(newMessage);
+      const messages = await db.ChatMessage.findAll({ where: { recipient_id: recipientId } });
+      return messages;
     } catch (error) {
-      console.error('Erreur lors de la création du message :', error);
-      res.status(500).json({ error: 'Erreur lors de la création du message' });
+      throw new Error('Unable to get all messages');
     }
   };
   
-  // Contrôleur pour récupérer tous les messages de chat
-exports.getAllMessages = async (req, res) => {
+  exports.saveMessage = async (message, senderId, recipientId) => {
     try {
-      // Récupérer l'ID du formateur depuis la requête
-      const formateurId = req.query.formateurId;
-
-      // Récupérer tous les messages de chat du formateur depuis la base de données
-      const messages = await ChatMessage.findAll({ where: { sender_id: formateurId } });
-
-      res.status(200).json(messages);
+      const savedMessage = await db.ChatMessage.create({ message, sender_id: senderId, recipient_id: recipientId });
+      return savedMessage;
     } catch (error) {
-      console.error('Erreur lors de la récupération des messages :', error);
-      res.status(500).json({ error: 'Erreur lors de la récupération des messages' });
+      throw new Error('Unable to save message');
     }
-};
-
-  
+  };
