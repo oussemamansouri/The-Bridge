e651d3ba2c6a56bfc9b58b6e5b75a22f7bfa33e3
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-02 02:53:51 +0100

    Create addpaiement.component.ts

---
+++ FrontEnd/src/app/views/admin/addpaiement/addpaiement/addpaiement.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addpaiement',
+  templateUrl: './addpaiement.component.html',
+  styleUrls: ['./addpaiement.component.scss']
+})
+export class AddpaiementComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
