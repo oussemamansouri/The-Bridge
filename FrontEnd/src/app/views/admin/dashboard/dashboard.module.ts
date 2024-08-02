3a6eff45541a86522d3ba36a80f0a166e1035785
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-02 02:53:38 +0100

    Create dashboard.module.ts

---
+++ FrontEnd/src/app/views/admin/dashboard/dashboard.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { DashboardRoutingModule } from './dashboard-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent
+  ],
+  imports: [
+    CommonModule,
+    DashboardRoutingModule,
+    FormsModule
+  ]
+})
+export class DashboardModule { }
