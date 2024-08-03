6382c75795233be8e620251b46df65ede4a66212
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-03 01:02:40 +0100

    Update data.service.ts

--- FrontEnd/src/app/views/services/data.service.ts
+++ FrontEnd/src/app/views/services/data.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -26,6 +27,9 @@ export class DataService {
     return this.http.patch(`http://localhost:3000/admin/updatepassword/${id}`,body) // ,{params:this.params,headers:this.header} |
    }
 
+
+
+
 // admin formateur
 
   getAllformateur(){
@@ -47,6 +51,20 @@ export class DataService {
   getoneformateur(id:any){
     return this.http.get('http://localhost:3000/formateur/profile/'+id)
   }
+  updateaformateurimage(img:any,id:any){
+    return this.http.patch(`http://localhost:3000/formateur/updateimage/${id}`,img)
+  }
+  updateaformateurcv(cv:any,id:any){
+    return this.http.patch(`http://localhost:3000/formateur/updatecv/${id}`,cv)
+  }
+
+  updateformateurf(body:any,id:any){
+    return this.http.patch(`http://localhost:3000/formateur/updateprofile/${id}`,body) // ,{params:this.params,headers:this.header} |
+   }
+
+   updatepasswordformateur(body:any,id:any){
+    return this.http.patch(`http://localhost:3000/formateur/updatepassword/${id}`,body) // ,{params:this.params,headers:this.header} |
+   }
   // fin  admin formateur
 
 
@@ -68,20 +86,32 @@ export class DataService {
     return this.http.patch('http://localhost:3000/moderateur/updateprofile/'+id,newprofile)
   }
 
-  getonemoderateur(id:any){
-    return this.http.get('http://localhost:3000/moderateur/profile/'+id)
+  getonemoderateur(id: any) {
+    return this.http.get('http://localhost:3000/moderateur/profile/'+ id);
   }
+
+  updateamoderatorimage(img:any,id:any){
+    return this.http.patch(`http://localhost:3000/moderateur/updateimage/${id}`,img)
+  }
+
+  updatemoderator(body:any,id:any){
+    return this.http.patch(`http://localhost:3000/moderateur/updateprofile/${id}`,body) // ,{params:this.params,headers:this.header} |
+   }
+
+   updatepasswordmoderateur(body:any,id:any){
+    return this.http.patch(`http://localhost:3000/moderateur/updatepassword/${id}`,body) // ,{params:this.params,headers:this.header} |
+   }
 //  fin admin moderateur
 
 // formateur formation
 
 getAllformation(){
-  return this.http.get('http://localhost:3000/moderateur/formations')
+  return this.http.get('http://localhost:3000/formation/formations')
 }
 
-addformation(f:any){
-  return this.http.post('http://localhost:3000/formation/addformation',f)
-}
+// addformation(f:any){
+//   return this.http.post('http://localhost:3000/formation/addformation',f)
+// }
 
 deleteformation(id:any){
   return this.http.delete('http://localhost:3000/formation/deleteformation/'+id)
@@ -95,4 +125,88 @@ getoneformation(id:any){
   return this.http.get('http://localhost:3000/formation/formation/'+id)
 }
 
+addformationparid(f: any, id: any) {
+  return this.http.post(`http://localhost:3000/formation/addformation/${id}`, f);
 }
+
+updateaformationimage(img:any,id:any){
+  return this.http.patch(`http://localhost:3000/formation/updateimage/${id}`,img)
+}
+// getAllFormationByFormateur(FormateurId: string){
+//   return this.http.get(`http://localhost:3000/formation/formations/${FormateurId}`);
+// }
+getAllFormationByFormateur(id: any){
+  return this.http.get(`http://localhost:3000/formation/formations/`+id);
+}
+
+// pack api
+
+addpack(f:any){
+  return this.http.post('http://localhost:3000/pack/addpack',f)
+}
+
+getAllpack(){
+  return this.http.get('http://localhost:3000/pack/packs')
+}
+
+deletepack(id:any){
+  return this.http.delete('http://localhost:3000/pack/deletepack/'+id)
+}
+updatepack(id:string,newpack:any){
+  return this.http.patch('http://localhost:3000/pack/updatepack/'+id,newpack)
+}
+getonepack(id:any){
+  return this.http.get('http://localhost:3000/pack/pack/'+id)
+}
+
+buyPoints(formateurId: number, points: number) {
+  return this.http.patch<any>(`http://localhost:3000/formateur/buyPoints/${formateurId}/${points}`, {})
+}
+
+
+
+////////////////////
+
+envoyerDemande(FormateurId: number, FormationId: number,ReceiverId:number ): Observable<any> {
+  return this.http.post<any>(`http://localhost:3000/demande/envoyer/${FormateurId}/${FormationId}/${ReceiverId}`, {});
+}
+
+getallDemandesByFormateurId(formateurId: number): Observable<any> {
+  return this.http.get<any>(`http://localhost:3000/demande/myDemandes/${formateurId}`);
+}
+
+
+
+getDemandesParticipation(formateurId: number): Observable<any> {
+  return this.http.get<any>(`http://localhost:3000/demande/RequestsReceive/${formateurId}`);
+}
+
+accepterRequest(FormateurId: number,FormationId: number,ReceiverId: number): Observable<any> {
+  return this.http.post<any>(`http://localhost:3000/participation/accepte/${FormateurId}/${FormationId}/${ReceiverId}`,{});
+}
+
+refuserRequest(FormateurId: number,FormationId: number,ReceiverId: number): Observable<any> {
+  return this.http.delete<any>(`http://localhost:3000/demande/delete/${FormateurId}/${FormationId}/${ReceiverId}`,{});
+}
+
+getParticipation(FormateurId: number): Observable<any> {
+  return this.http.get<any>(`http://localhost:3000/participation/myParticipation/${FormateurId}`,{});
+}
+
+
+ddeleteParticipation(FormateurId: number,FormationId: number,ReceiverId: number): Observable<any> {
+  return this.http.delete<any>(`http://localhost:3000/participation/deleteparticipation/${FormateurId}/${FormationId}/${ReceiverId}`,{});
+}
+
+
+
+
+
+getAcceptedFriends(formateurId: number): Observable<any> {
+  return this.http.get<any>(`http://localhost:3000/participation/acceptedFriends/${formateurId}`);
+}
+
+
+}
+
+
