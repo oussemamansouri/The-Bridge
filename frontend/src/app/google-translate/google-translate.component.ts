e14393a99c9dced8083150f67ecf0f151dc54aea
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-03 23:25:52 +0100

    Create google-translate.component.ts

---
+++ frontend/src/app/google-translate/google-translate.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-google-translate',
+  templateUrl: './google-translate.component.html',
+  styleUrls: ['./google-translate.component.scss']
+})
+export class GoogleTranslateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
