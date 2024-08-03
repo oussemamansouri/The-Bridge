c245d96938c86e613d3e421a08dea7fc18f2906b
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-04 00:40:23 +0100

    Update moderator-layout.component.ts

--- FrontEnd/src/app/layouts/moderator-layout/moderator-layout.component.ts
+++ FrontEnd/src/app/layouts/moderator-layout/moderator-layout.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { AuthadminService } from 'src/app/views/services/authadmin.service';
 
 @Component({
   selector: 'app-moderator-layout',
@@ -7,13 +9,26 @@ import { Router } from '@angular/router';
   styleUrls: ['./moderator-layout.component.scss']
 })
 export class ModeratorLayoutComponent implements OnInit {
+  username:any
+  data: any;
+  helper= new JwtHelperService
+  imagepath:any='http://localhost:3000/'
 
-  constructor(private route:Router) { }
+
+  constructor(private route:Router) {
+    this.username=localStorage.getItem('username')
+   }
  
   ngOnInit(): void {
+    const token = localStorage.getItem('token');
+    if (token) {
+      const decodedToken = this.helper.decodeToken(token);
+      this.data = decodedToken;
+    }else{this.data=''}
   }
   logout(){
     localStorage.removeItem('token')
     this.route.navigate(['/loginuser'])
+    this.ngOnInit()
   }
 }
