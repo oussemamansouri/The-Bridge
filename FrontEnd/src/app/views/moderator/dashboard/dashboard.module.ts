05ec99b36d3d349709d83848becb5d6efe2f4b8d
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-03 01:05:57 +0100

    Update dashboard.module.ts

--- FrontEnd/src/app/views/moderator/dashboard/dashboard.module.ts
+++ FrontEnd/src/app/views/moderator/dashboard/dashboard.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   ],
   imports: [
     CommonModule,
-    DashboardRoutingModule
+    DashboardRoutingModule,
+    FormsModule
   ]
 })
 export class DashboardModule { }
