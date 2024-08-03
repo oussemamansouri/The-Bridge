23eb21db397019dbc06a5a7d77d8e3b483eb3574
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-03 01:04:56 +0100

    Update detailsformateur.component.ts

--- FrontEnd/src/app/views/moderator/detailsformateur/detailsformateur/detailsformateur.component.ts
+++ FrontEnd/src/app/views/moderator/detailsformateur/detailsformateur/detailsformateur.component.ts
@@ -9,10 +9,13 @@ import { DataService } from 'src/app/views/services/data.service';
   styleUrls: ['./detailsformateur.component.scss']
 })
 export class DetailsformateurComponent implements OnInit {
-
+cv:any
   id: string = '';
   dataobject:any
   messageerr=''
+  imagepath:any='http://localhost:3000/'
+  cvpathe:any='http://localhost:3000/'
+
   constructor(private route:ActivatedRoute,private ds:DataService) {
     this.route.params.subscribe((params: Params) => this.id = params['id']);
 
@@ -23,5 +26,15 @@ export class DetailsformateurComponent implements OnInit {
    }
 
   ngOnInit(): void {
+  }
+
+  openCV() {
+    window.open(this.cvpathe + this.dataobject.cv, '_blank');
+  }
+  downloadCV() {
+    const link = document.createElement('a');
+    link.href = this.cvpathe + this.dataobject.cv;
+    link.download = this.dataobject.cv;  // Ici, nous mettons juste le nom du fichier
+    link.click();
   }
 }
