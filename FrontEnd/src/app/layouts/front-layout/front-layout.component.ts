22f1925524c5fdb2decab521556e6fc65995c4b5
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-04 00:40:18 +0100

    Update front-layout.component.ts

--- FrontEnd/src/app/layouts/front-layout/front-layout.component.ts
+++ FrontEnd/src/app/layouts/front-layout/front-layout.component.ts
@@ -1,7 +1,9 @@
+
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { AuthLoginService } from 'src/app/views/services/auth-login.service';
+import { DataService } from 'src/app/views/services/data.service'; 
 
 @Component({
   selector: 'app-front-layout',
@@ -9,42 +11,80 @@ import { AuthLoginService } from 'src/app/views/services/auth-login.service';
   styleUrls: ['./front-layout.component.scss']
 })
 export class FrontLayoutComponent implements OnInit {
-  data: any;
-  helper= new JwtHelperService
-  imagepath:any='http://localhost:3000/'
+  data: any = ''; 
+  helper: JwtHelperService = new JwtHelperService();
+  imagepath: string = 'http://localhost:3000/'; 
+  profile: any; 
+    selectedCity: string = 'TN';
+  cities = [
+    { name: 'Tunisie', abbr: 'TN' },
+    { name: 'France', abbr: 'FR' },
+    { name: 'New York', abbr: 'NY' }
+    // Ajoutez d'autres villes ici
+  ];
+  http: any;
 
-  constructor(public asf:AuthLoginService,private router:Router) {
-
+  selectCity(cityAbbr: string): void {
+    this.selectedCity = cityAbbr;
   }
+  selectedLanguage: string = 'FR';
+  languages = [
+    { name: 'Français', abbr: 'FR' },
+    { name: 'English', abbr: 'EN' },
+    { name: 'العربية', abbr: 'AR' }
+  ];
+
+  constructor(public asf: AuthLoginService, private router: Router, private api: DataService) { } // Fusionnez les constructeurs en un seul
 
   ngOnInit(): void {
     const token = localStorage.getItem('token');
     if (token) {
       const decodedToken = this.helper.decodeToken(token);
       this.data = decodedToken;
-    }else{this.data=''}
-
-  }
-  logout(){
-    localStorage.removeItem('token')
-    this.router.navigate(['/'])
-    this.ngOnInit()
-  }
-
-  navigateprofile(){
-    switch(this.data.role) {
-      case 'Admin':
-        this.router.navigate(['/admin'])
-        break;
-      case 'modirateur':
-        this.router.navigate(['/moderator'])
-        break;
-        case 'formateur':
-          this.router.navigate(['/formateur'])
-          break;
-      default:
-        this.router.navigate(['/loginuser'])
     }
 
+    const id = this.getId();
+    this.api.getoneformateur(id).subscribe(data => {
+      this.profile = data;
+      console.log(this.profile);
+    });
   }
+
+  getId():number{
+    let token:any=localStorage.getItem('token')
+   let decodedtoken:any=this.helper.decodeToken(token)
+    return decodedtoken.id
+  }
+
+
+  logout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/']);
+    this.ngOnInit();
+  }
+
+  navigateprofile() {
+    switch (this.data.role) {
+      case 'Admin':
+        this.router.navigate(['/admin']);
+        break;
+      case 'moderateur':
+        this.router.navigate(['/moderator']);
+        break;
+      case 'formateur':
+        this.router.navigate(['/formateur']);
+        break;
+      default:
+        this.router.navigate(['/loginuser']);
+    }
+  }
+  redirectToPacksPage() {
+    this.router.navigate(['/tarifs']);
+}
+selectLanguage(languageAbbr: string): void {
+  this.selectedLanguage = languageAbbr;
+  this.http.get(`https://translation.googleapis.com/language/translate/v2?key=YOUR_API_KEY&q=TEXT_TO_TRANSLATE&source=SOURCE_LANGUAGE&target=${languageAbbr}`).subscribe((response: any) => {
+
+  });
+}
 }
