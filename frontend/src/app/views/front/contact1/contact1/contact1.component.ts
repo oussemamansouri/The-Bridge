0282b82f75c44bf1695eab0f6ebb016e72d1e6e1
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-05 00:57:17 +0100

    Create contact1.component.ts

---
+++ frontend/src/app/views/front/contact1/contact1/contact1.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact1',
+  templateUrl: './contact1.component.html',
+  styleUrls: ['./contact1.component.scss']
+})
+export class Contact1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
