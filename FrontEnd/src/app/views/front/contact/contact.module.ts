aa34a2d142ff7773551a4454ce2d79ef1826b135
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-05 00:57:46 +0100

    Update contact.module.ts

--- FrontEnd/src/app/views/front/contact/contact.module.ts
+++ FrontEnd/src/app/views/front/contact/contact.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { ContactRoutingModule } from './contact-routing.module';
 import { ContactComponent } from './contact/contact.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { ContactComponent } from './contact/contact.component';
   ],
   imports: [
     CommonModule,
-    ContactRoutingModule
+    ContactRoutingModule,
+    FormsModule
   ]
 })
 export class ContactModule { }
