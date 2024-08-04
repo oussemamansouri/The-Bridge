77344300c191d1b478539802b80e5a307896e692
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-05 00:57:10 +0100

    Create profiledetailles.component.ts

---
+++ frontend/src/app/views/formateur/profiledetailles/profiledetailles/profiledetailles.component.ts
@@ -0,0 +1,42 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { DataService } from 'src/app/views/services/data.service';
+
+@Component({
+  selector: 'app-profiledetailles',
+  templateUrl: './profiledetailles.component.html',
+  styleUrls: ['./profiledetailles.component.scss']
+})
+export class ProfiledetaillesComponent implements OnInit {
+  id: string = '';
+  cv:any;
+  dataobject:any
+  messageerr=''
+  imagepath:any='http://localhost:3000/'
+  cvpathe:any='http://localhost:3000/'
+  constructor(private route:ActivatedRoute,private ds:DataService) { 
+    this.route.params.subscribe((params: Params) => this.id = params['id']);
+
+    this.ds.getoneformateur(this.id).subscribe(response=>this.dataobject=response,(err:HttpErrorResponse)=>{
+      console.log(err)
+      this.messageerr="we dont't found this student in our database"})
+    
+  }
+
+  ngOnInit(): void {
+  }
+
+
+  openCV() {
+    window.open(this.cvpathe + this.dataobject.cv, '_blank');
+  }
+
+  
+  downloadCV() {
+    const link = document.createElement('a');
+    link.href = this.cvpathe + this.dataobject.cv;
+    link.download = this.dataobject.cv;  // Ici, nous mettons juste le nom du fichier
+    link.click();
+  }
+}
