900b11fac4d1a632e66e3f02b64fc0ecaefdd8d7
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-06 00:22:45 +0100

    Update addpack.component.ts

--- FrontEnd/src/app/views/moderator/addpack/addpack/addpack.component.ts
+++ FrontEnd/src/app/views/moderator/addpack/addpack/addpack.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/views/services/data.service';
 
 @Component({
   selector: 'app-addpack',
@@ -7,9 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddpackComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ds:DataService,private route:Router) { }
 
   ngOnInit(): void {
+  }
+
+  addpack(f:any){
+    let data=f.value 
+    console.log(data)
+    this.ds.addpack(data).subscribe(data=>{
+      this.route.navigate(['/moderator/consultpack'])
+
+    })
+
   }
 
 }
