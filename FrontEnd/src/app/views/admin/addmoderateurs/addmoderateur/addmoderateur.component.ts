461b569a9471f6f2252bb5184130d5fba1fd2458
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-28 21:26:07 +0100

    Update addmoderateur.component.ts

--- FrontEnd/src/app/views/admin/addmoderateurs/addmoderateur/addmoderateur.component.ts
+++ FrontEnd/src/app/views/admin/addmoderateurs/addmoderateur/addmoderateur.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { DataService } from 'src/app/views/services/data.service';
 
-
 @Component({
   selector: 'app-addmoderateur',
   templateUrl: './addmoderateur.component.html',
@@ -10,18 +9,24 @@ import { DataService } from 'src/app/views/services/data.service';
 })
 export class AddmoderateurComponent implements OnInit {
 
-  constructor(private ds:DataService,private route:Router) { }
+  // Constructor: Injects DataService for handling data operations and Router for navigation
+  constructor(private ds: DataService, private route: Router) { }
 
+  // Lifecycle hook: Executes when the component is initialized
   ngOnInit(): void {
   }
 
-  addmoderateur(f:any){
-    let data=f.value 
-    console.log(data)
-    this.ds.addmoderateur(data).subscribe(data=>{
-      this.route.navigate(['/admin/consultmoderateur'])
+  // Method to add a new moderator
+  addmoderateur(f: any) {
+    // Extract the form data
+    let data = f.value;
 
-    })
+    // Log the data to the console (for debugging purposes)
+    console.log(data);
 
+    // Call the addmoderateur method from the DataService and navigate to the 'consult moderator' page upon success
+    this.ds.addmoderateur(data).subscribe(data => {
+      this.route.navigate(['/admin/consultmoderateur']);
+    });
   }
 }
