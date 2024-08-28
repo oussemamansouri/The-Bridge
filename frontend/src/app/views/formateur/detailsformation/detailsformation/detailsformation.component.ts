4514ea6ab99ac88a8aeea74ed2886f1e2cef9bff
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-29 00:35:56 +0100

    Update detailsformation.component.ts

--- frontend/src/app/views/formateur/detailsformation/detailsformation/detailsformation.component.ts
+++ frontend/src/app/views/formateur/detailsformation/detailsformation/detailsformation.component.ts
@@ -10,19 +10,31 @@ import { DataService } from 'src/app/views/services/data.service';
 })
 export class DetailsformationComponent implements OnInit {
 
-  id: string = '';
-  dataobject:any
-  messageerr=''
-  imagepath:any='http://localhost:3000/'
-  constructor(private route:ActivatedRoute,private ds:DataService) {
-    this.route.params.subscribe((params: Params) => this.id = params['id']);
+  id: string = ''; // ID of the formation to be displayed
+  dataobject: any; // Object to hold formation details
+  messageerr: string = ''; // Error message if formation is not found
+  imagepath: string = 'http://localhost:3000/'; // Base path for images
 
-    this.ds.getoneformation(this.id).subscribe(response=>this.dataobject=response,(err:HttpErrorResponse)=>{
-      console.log(err)
-      this.messageerr="we dont't found this formation in our database"})
-    
-   }
+  constructor(private route: ActivatedRoute, private ds: DataService) { }
+
   ngOnInit(): void {
+    // Subscribe to route parameters to get the formation ID
+    this.route.params.subscribe((params: Params) => {
+      this.id = params['id']; // Extract the ID from route parameters
+      this.loadFormation(); // Load the formation details
+    });
   }
 
+  // Method to load formation details from the server
+  loadFormation(): void {
+    this.ds.getoneformation(this.id).subscribe(
+      response => {
+        this.dataobject = response; // Assign formation details to dataobject
+      },
+      (err: HttpErrorResponse) => {
+        console.log(err); // Log error for debugging
+        this.messageerr = "We couldn't find this formation in our database"; // Set error message
+      }
+    );
+  }
 }
