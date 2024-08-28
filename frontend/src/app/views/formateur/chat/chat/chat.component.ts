11f6030e3bb10547dc8d8b22bc954bb56ff319e4
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-29 00:35:39 +0100

    Update chat.component.ts

--- frontend/src/app/views/formateur/chat/chat/chat.component.ts
+++ frontend/src/app/views/formateur/chat/chat/chat.component.ts
@@ -1,93 +1,3 @@
-// import { Component, OnInit } from '@angular/core';
-// import { JwtHelperService } from '@auth0/angular-jwt';
-// import { io } from 'socket.io-client';
-// import { ChatService } from 'src/app/views/services/chat.service';
-// import { DataService } from 'src/app/views/services/data.service';
-
-
-// @Component({
-//   selector: 'app-chat',
-//   templateUrl: './chat.component.html',
-//   styleUrls: ['./chat.component.scss']
-// })
-// export class ChatComponent implements OnInit {
-//   myParticipation: any = [];
-// recipientId = 2; // Example recipient ID
-// messages: any[] = [];
-// newMessage: string = '';
-// myId: any;
-// profile : any
-//   id:any
-//   img:any
-//   imagepath:any='http://localhost:3000/'
-//   helper= new JwtHelperService
-//   dataArray: any = [];
-// constructor(private chatService: ChatService,private ds:DataService) { 
-//   this.ds.getAllformateur().subscribe(data => {
-//     console.log(data);
-//     this.dataArray = data;
-//   });
-// }
-
-
-
-// ngOnInit(): void {
-//   this.myId = this.getId();
-//   const id = this.getId();
-//   this.ds.getoneformateur(id).subscribe(data => {
-//     this.profile = data;
-//     console.log(this.profile);
-//   });
-
-//   this.loadMessages();
-//   this.loadParticipations();
-// }
-
-
-
-
-// loadMessages() {
-//   this.chatService.getAllMessages(this.recipientId).subscribe(
-//     (response) => {
-//       this.messages = response.messages;
-//     },
-//     (error) => {
-//       console.error('Error loading messages:', error);
-//     }
-//   );
-// }
-
-// sendMessage() {
-//   if (this.newMessage.trim() === '') {
-//     return; // Don't send empty messages
-//   }
-//   this.chatService.sendMessage(this.newMessage, this.getId() /* Example sender ID */, this.recipientId).subscribe(
-//     (response) => {
-//       this.messages.push(response.message);
-//       this.newMessage = ''; // Clear the input field after sending
-//     },
-//     (error) => {
-//       console.error('Error sending message:', error);
-//     }
-//   );
-// }
-
-
-// getId():number{
-//   let token:any=localStorage.getItem('token')
-//  let decodedtoken:any=this.helper.decodeToken(token)
-//   return decodedtoken.id
-// }
-
-
-// loadParticipations() {
-//   this.ds.getParticipation(this.myId).subscribe(data => {
-//     this.myParticipation = data;
-//     console.log(this.myParticipation)
-//   });
-// }
-// }
-
 import { Component, OnInit } from '@angular/core';
 import { JwtHelperService } from '@auth0/angular-jwt';
 import { ChatService } from 'src/app/views/services/chat.service';
@@ -99,29 +9,32 @@ import { DataService } from 'src/app/views/services/data.service';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent implements OnInit {
-  myParticipation: any = [];
-  recipientId = 9; // Exemple d'ID de destinataire, peut être mis à jour lorsqu'un participant est sélectionné
-  messages: any[] = [];
-  newMessage: string = '';
-  myId: any;
-  profile: any;
-  imagepath: string = 'http://localhost:3000/';
-  helper = new JwtHelperService();
-  dataArray: any = [];
-  acceptedFriends: any[] = [];
-  selectedFriend: any = {};
+  myParticipation: any = []; // Array to hold the list of my participations
+  recipientId = 9; // Default recipient ID, can be updated when a participant is selected
+  messages: any[] = []; // Array to store chat messages
+  newMessage: string = ''; // Variable to hold the new message input
+  myId: any; // Variable to store the current user's ID
+  profile: any; // Variable to store user profile information
+  imagepath: string = 'http://localhost:3000/'; // Base URL for image paths
+  helper = new JwtHelperService(); // JWT helper for decoding tokens
+  dataArray: any = []; // Variable to store additional data (not used in current code)
+  acceptedFriends: any[] = []; // Array to store accepted friends
+  selectedFriend: any = {}; // Variable to store selected friend information (not used in current code)
 
   constructor(private chatService: ChatService, private ds: DataService) { }
 
   ngOnInit(): void {
-    this.myId = this.getId();
+    this.myId = this.getId(); // Retrieve the current user's ID
+    // Load the user's profile information
     this.ds.getoneformateur(this.myId).subscribe(data => {
       this.profile = data;
     });
 
+    // Load chat messages and participation data
     this.loadMessages();
     this.loadParticipations();
 
+    // Load accepted friends
     this.ds.getAcceptedFriends(this.getId()).subscribe(
       (data) => {
         this.acceptedFriends = data;
@@ -132,10 +45,11 @@ export class ChatComponent implements OnInit {
     );
   }
 
+  // Method to load chat messages with the current recipient
   loadMessages() {
     this.chatService.getAllMessages(this.recipientId).subscribe(
       (response) => {
-        this.messages = response.messages;
+        this.messages = response.messages; // Assign received messages to the messages array
       },
       (error) => {
         console.error('Erreur lors du chargement des messages :', error);
@@ -143,16 +57,17 @@ export class ChatComponent implements OnInit {
     );
   }
 
+  // Method to send a new chat message
   sendMessage() {
     if (this.newMessage.trim() === '') {
-      return; // Ne pas envoyer de messages vides
+      return; // Do not send empty messages
     }
-    console.log('Envoi du message :', this.newMessage); // Débogage
+    console.log('Envoi du message :', this.newMessage); // Debugging
     this.chatService.sendMessage(this.newMessage, this.myId, this.recipientId).subscribe(
       (response) => {
-        console.log('Réponse du serveur :', response); // Débogage
-        this.messages.push(response.message);
-        this.newMessage = ''; // Effacer le champ de saisie après l'envoi
+        console.log('Réponse du serveur :', response); // Debugging
+        this.messages.push(response.message); // Add the new message to the messages array
+        this.newMessage = ''; // Clear the input field after sending
       },
       (error) => {
         console.error('Erreur lors de l\'envoi du message :', error);
@@ -160,21 +75,23 @@ export class ChatComponent implements OnInit {
     );
   }
 
+  // Method to retrieve the current user's ID from the JWT token
   getId(): number {
     let token: any = localStorage.getItem('token');
     let decodedtoken: any = this.helper.decodeToken(token);
-    return decodedtoken.id;
+    return decodedtoken.id; // Return the ID from the decoded token
   }
 
+  // Method to load participation data
   loadParticipations() {
     this.ds.getParticipation(this.myId).subscribe(data => {
-      this.myParticipation = data;
+      this.myParticipation = data; // Assign the participation data to the myParticipation array
     });
   }
 
+  // Method to select a participant and load messages with them
   selectParticipant(friendid: number) {
-    this.recipientId = friendid;
-    this.loadMessages();
+    this.recipientId = friendid; // Set the recipient ID to the selected participant's ID
+    this.loadMessages(); // Load messages with the newly selected recipient
   }
 }
-
