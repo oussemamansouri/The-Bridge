b4548cc144b7e9b60a90f72058e0903d79a32b46
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-28 14:15:40 +0100

    Update moderateur_router.js

--- BackEnd/routers/moderateur_router.js
+++ BackEnd/routers/moderateur_router.js
@@ -1,81 +1,81 @@
-const express=require('express')
-const route=express.Router()
-const db=require('../models')
-const moderateur_controller=require('../controllers/moderateur_controller')
-// const jwt=require('jsonwebtoken')
-// require('dotenv').config()
-const path=require('path')
+const express = require('express');
+const route = express.Router();
+const db = require('../models');
+const moderateur_controller = require('../controllers/moderateur_controller');
+const path = require('path');
 
+//////////////////////////////  Register API  ///////////////////////////////////////////////
 
-
-
-
-//////////////////////////////  register api start here ///////////////////////////////////////////////:
-
+// Route to handle moderator registration
 route.post('/register', moderateur_controller.upload, (req, res) => {
 
-const defaultImg = 'assets/image/default-image.jpg';
-let imgPath = defaultImg;
-    if (req.files && req.files['img'] && req.files['img'][0]) { // Vérifie si une image a été téléchargée
+    const defaultImg = 'assets/image/default-image.jpg';
+    let imgPath = defaultImg;
+    
+    // Check if an image was uploaded and set the image path accordingly
+    if (req.files && req.files['img'] && req.files['img'][0]) {
         imgPath = req.files['img'][0].path;
     }
 
-    moderateur_controller.register(req.body.firstname,req.body.lastname,req.body.username,req.body.email,req.body.password,imgPath,req.body.tel)
-    .then(response=>res.status(200).json(response))
-    .catch(err=>res.status(400).json(err))     
-    
-})
+    // Call the register function from the controller with the form data and image path
+    moderateur_controller.register(req.body.firstname, req.body.lastname, req.body.username, req.body.email, req.body.password, imgPath, req.body.tel)
+        .then(response => res.status(200).json(response)) // Send success response
+        .catch(err => res.status(400).json(err)); // Send error response
+});
 
-route.delete('/deleteprofile/:id',moderateur_controller.DeleteProfile)
+//////////////////////////////  Delete Profile API  ///////////////////////////////////////////////
 
+// Route to handle the deletion of a moderator's profile
+route.delete('/deleteprofile/:id', moderateur_controller.DeleteProfile);
 
-route.get('/profiles',(req,res,next)=>{ 
+//////////////////////////////  Get All Profiles API  ///////////////////////////////////////////////
 
+// Route to get all moderator profiles
+route.get('/profiles', (req, res, next) => {
     db.Moderateur.findAll()
-    .then((response)=>res.status(200).json(response))
-    .catch((err)=>res.status(400).json(err))
-    
-})
+        .then(response => res.status(200).json(response)) // Send success response with all profiles
+        .catch(err => res.status(400).json(err)); // Send error response
+});
 
+//////////////////////////////  Get Profile by ID API  ///////////////////////////////////////////////
 
-    route.get('/profile/:id',(req,res,next)=>{ 
+// Route to get a specific moderator's profile by ID
+route.get('/profile/:id', (req, res, next) => {
+    db.Moderateur.findOne({ where: { id: req.params.id } })
+        .then(response => res.status(200).json(response)) // Send success response with the profile data
+        .catch(err => res.status(400).json(err)); // Send error response
+});
 
-        db.Moderateur.findOne({where:{id:req.params.id}})
-        .then((response)=>res.status(200).json(response))
-        .catch((err)=>res.status(400).json(err))
-        
-    })
+//////////////////////////////  Update Profile API  ///////////////////////////////////////////////
 
+// Route to update a moderator's profile
+route.patch('/updateprofile/:id', (req, res) => {
+    moderateur_controller.updateprofile(req.body.firstname, req.body.lastname, req.body.username, req.body.email, req.body.tel, req.params.id)
+        .then(response => res.status(200).json(response)) // Send success response after profile update
+        .catch(err => res.status(400).json(err)); // Send error response
+});
 
-route.patch('/updateprofile/:id',(req,res)=>{
-    moderateur_controller.updateprofile(req.body.firstname,req.body.lastname,req.body.username,req.body.email,req.body.tel,req.params.id)
-    .then(response=>res.status(200).json(response))
-    .catch(err=>res.status(400).json(err))
-})
+//////////////////////////////  Update Password API  ///////////////////////////////////////////////
 
-
-
-//////////////////////////////////   update password api start here     //////////////////////////////////////////
-
+// Route to update a moderator's password
 route.patch('/updatepassword/:id', (req, res) => {
     moderateur_controller.updatepassword(
-      req.body.oldPassword,
-      req.body.newPassword,
-      req.body.repeatPassword,
-      req.params.id
+        req.body.oldPassword,
+        req.body.newPassword,
+        req.body.repeatPassword,
+        req.params.id
     )
-      .then(response => res.status(200).json(response))
-      .catch(err => res.status(400).json(err))
-  })
-//////////////////////////////////   update password api end here     //////////////////////////////////////////
+        .then(response => res.status(200).json(response)) // Send success response after password update
+        .catch(err => res.status(400).json(err)); // Send error response
+});
 
+//////////////////////////////  Update Image API  ///////////////////////////////////////////////
 
-  
+// Route to update a moderator's profile image
+route.patch('/updateimage/:id', moderateur_controller.uploadimg, (req, res) => {
+    moderateur_controller.updateimage(req.file.path, req.params.id)
+        .then(response => res.status(200).json(response)) // Send success response after image update
+        .catch(err => res.status(400).json(err)); // Send error response
+});
 
-    route.patch('/updateimage/:id', moderateur_controller.uploadimg, (req, res) => {
-        moderateur_controller.updateimage(req.file.path, req.params.id)
-          .then(response => res.status(200).json(response))
-          .catch(err => res.status(400).json(err))
-      })
-
-module.exports=route
+module.exports = route;
