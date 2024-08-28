132beebd78c7e90f3523049131a30dfb9caca1db
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-28 14:15:34 +0100

    Update formateur_router.js

--- BackEnd/routers/formateur_router.js
+++ BackEnd/routers/formateur_router.js
@@ -1,43 +1,52 @@
-const express=require('express')
-const route=express.Router()
-const db=require('../models')
-const formateur_controller=require('../controllers/formateur_controller')
-// const jwt=require('jsonwebtoken')
-// require('dotenv').config()
-const path=require('path')
+const express = require('express');
+const route = express.Router();
+const db = require('../models');
+const formateur_controller = require('../controllers/formateur_controller');
+const path = require('path');
 
+//////////////////////////////  Register API  ///////////////////////////////////////////////
 
-
-
-
-//////////////////////////////  register api start here ///////////////////////////////////////////////:
-
+// Route to register a new formateur (trainer)
+// It handles image and CV upload, and assigns default images if not provided
 route.post('/register', formateur_controller.upload, (req, res) => {
-
+    // Set default image path
     const defaultImg = 'assets/image/default-image.jpg';
     let imgPath = defaultImg;
-    if (req.files && req.files['img'] && req.files['img'][0]) { // Vérifie si une image a été téléchargée
+    if (req.files && req.files['img'] && req.files['img'][0]) { // Check if an image was uploaded
         imgPath = req.files['img'][0].path;
     }
     
+    // Set default CV path
     const defaultCv = 'assets/image/default-cv-image.jpg';
     let cvPath = defaultCv;
-    if (req.files && req.files['cv'] && req.files['cv'][0]) { // Vérifie si un cv a été téléchargé
+    if (req.files && req.files['cv'] && req.files['cv'][0]) { // Check if a CV was uploaded
         cvPath = req.files['cv'][0].path;
     }
 
+    // Call the register function from formateur_controller with the provided data
+    formateur_controller.register(
+        req.body.firstname,
+        req.body.lastname,
+        req.body.email,
+        req.body.password,
+        imgPath,
+        req.body.tel,
+        req.body.dob,
+        req.body.address,
+        cvPath,
+        req.body.portfolio,
+        req.body.statu,
+        req.body.linkedin,
+        req.body.niveau,
+        req.body.experience
+    )
+    .then(response => res.status(200).json(response)) // Send success response with status 200
+    .catch(err => res.status(400).json(err)); // Send error response with status 400
+});
 
-formateur_controller.register(req.body.firstname,req.body.lastname,req.body.email,req.body.password,imgPath,req.body.tel,req.body.dob,req.body.address,cvPath,req.body.portfolio,req.body.statu,req.body.linkedin,req.body.niveau,req.body.experience)
-    .then(response=>res.status(200).json(response))
-    .catch(err=>res.status(400).json(err))     
+//////////////////////////////////   Update Password API  //////////////////////////////////////////
 
-})
-
-
-
-
-//////////////////////////////////   update password api start here     //////////////////////////////////////////
-
+// Route to update the password of a formateur
 route.patch('/updatepassword/:id', (req, res) => {
     formateur_controller.updatepassword(
       req.body.oldPassword,
@@ -45,63 +54,73 @@ route.patch('/updatepassword/:id', (req, res) => {
       req.body.repeatPassword,
       req.params.id
     )
-      .then(response => res.status(200).json(response))
-      .catch(err => res.status(400).json(err))
-  })
+    .then(response => res.status(200).json(response)) // Send success response with status 200
+    .catch(err => res.status(400).json(err)); // Send error response with status 400
+});
 
-//////////////////////////////////   update password api end here     //////////////////////////////////////////
+//////////////////////////////////   Update Profile API  //////////////////////////////////////////
 
+// Route to update the profile of a formateur
+route.patch('/updateprofile/:id', (req, res) => {
+    formateur_controller.updateprofile(
+        req.body.firstname,
+        req.body.lastname,
+        req.body.dob,
+        req.body.address,
+        req.body.tel,
+        req.body.portfolio,
+        req.body.statu,
+        req.body.linkedin,
+        req.body.niveau,
+        req.body.experience,
+        req.params.id
+    )
+    .then(response => res.status(200).json(response)) // Send success response with status 200
+    .catch(err => res.status(400).json(err)); // Send error response with status 400
+});
 
-    route.patch('/updateprofile/:id',(req,res)=>{
-    formateur_controller.updateprofile(req.body.firstname,req.body.lastname,req.body.dob,req.body.address,
-    req.body.tel,req.body.portfolio,req.body.statu,req.body.linkedin,req.body.niveau,req.body.experience,req.params.id)
-    .then(response=>res.status(200).json(response))
-    .catch(err=>res.status(400).json(err))
-    })
+// Route to update the profile image of a formateur
+route.patch('/updateimage/:id', formateur_controller.uploadimg, (req, res) => {
+    formateur_controller.updateimage(req.file.path, req.params.id)
+    .then(response => res.status(200).json(response)) // Send success response with status 200
+    .catch(err => res.status(400).json(err)); // Send error response with status 400
+});
 
+// Route to update the CV of a formateur
+route.patch('/updatecv/:id', formateur_controller.uploadcv, (req, res) => {
+    formateur_controller.updatecv(req.file.path, req.params.id)
+    .then(response => res.status(200).json(response)) // Send success response with status 200
+    .catch(err => res.status(400).json(err)); // Send error response with status 400
+});
 
-    route.patch('/updateimage/:id', formateur_controller.uploadimg, (req, res) => {
-        formateur_controller.updateimage(req.file.path, req.params.id)
-          .then(response => res.status(200).json(response))
-          .catch(err => res.status(400).json(err))
-      })
+//////////////////////////////////   Delete Profile API  //////////////////////////////////////////
 
+// Route to delete a formateur's profile
+route.delete('/deleteprofile/:id', formateur_controller.DeleteProfile);
 
-      route.patch('/updatecv/:id', formateur_controller.uploadcv, (req, res) => {
-        formateur_controller.updatecv(req.file.path, req.params.id)
-          .then(response => res.status(200).json(response))
-          .catch(err => res.status(400).json(err))
-      })
+//////////////////////////////////   Get Profiles API  //////////////////////////////////////////
 
+// Route to get all formateur profiles
+route.get('/profiles', (req, res, next) => {
+    db.Formateur.findAll()
+    .then(response => res.status(200).json(response)) // Send success response with status 200
+    .catch(err => res.status(400).json(err)); // Send error response with status 400
+});
 
+// Route to get a specific formateur profile by ID
+route.get('/profile/:id', (req, res) => {
+    db.Formateur.findOne({ where: { id: req.params.id } })
+    .then(response => res.status(200).json(response)) // Send success response with status 200
+    .catch(err => res.status(400).json(err)); // Send error response with status 400
+});
 
+//////////////////////////////////   Buy Points API  //////////////////////////////////////////
 
-    route.delete('/deleteprofile/:id', formateur_controller.DeleteProfile)
+// Route to allow a formateur to buy points
+route.patch('/buyPoints/:id/:points', (req, res) => {
+    formateur_controller.buyPoints(req.params.id, req.params.points)
+    .then(response => res.status(200).json(response)) // Send success response with status 200
+    .catch(err => res.status(400).json(err)); // Send error response with status 400
+});
 
-
-    route.get('/profiles',(req,res,next)=>{
-
-        db.Formateur.findAll()
-        .then((response)=>res.status(200).json(response))
-        .catch((err)=>res.status(400).json(err))
-        
-        })
-
-
-        route.get('/profile/:id',(req,res)=>{ 
-
-            db.Formateur.findOne({where:{id:req.params.id}})
-            .then((response)=>res.status(200).json(response))
-            .catch((err)=>res.status(400).json(err))
-            
-            })
-
-            route.patch('/buyPoints/:id/:points',(req,res)=>{ 
-
-              formateur_controller.buyPoints(req.params.id,req.params.points)
-              .then((response)=>res.status(200).json(response))
-              .catch((err)=>res.status(400).json(err))
-              
-              })
-
-module.exports=route
+module.exports = route;
