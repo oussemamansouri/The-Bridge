ba64fd9a5be4a41a19564fd114d33c7122fb2d97
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-28 21:25:57 +0100

    Update addformateur.component.ts

--- FrontEnd/src/app/views/admin/addformateur/addformateur/addformateur.component.ts
+++ FrontEnd/src/app/views/admin/addformateur/addformateur/addformateur.component.ts
@@ -9,19 +9,24 @@ import { DataService } from 'src/app/views/services/data.service';
 })
 export class AddformateurComponent implements OnInit {
 
-  constructor(private ds:DataService,private route:Router) { }
+  // Constructor: Injects DataService for handling data operations and Router for navigation
+  constructor(private ds: DataService, private route: Router) { }
 
+  // Lifecycle hook: Executes when the component is initialized
   ngOnInit(): void {
   }
 
-  addformateur(f:any){
-    let data=f.value 
-    console.log(data)
-    this.ds.addformateur(data).subscribe(data=>{
-      this.route.navigate(['/admin/consultformateur'])
+  // Method to add a new formateur (instructor)
+  addformateur(f: any) {
+    // Extract the form data
+    let data = f.value;
 
-    })
+    // Log the data to the console (for debugging purposes)
+    console.log(data);
 
+    // Call the addformateur method from the DataService and navigate to the 'consult formateur' page upon success
+    this.ds.addformateur(data).subscribe(data => {
+      this.route.navigate(['/admin/consultformateur']);
+    });
   }
-
 }
