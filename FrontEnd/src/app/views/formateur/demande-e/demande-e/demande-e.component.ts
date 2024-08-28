3d1ab674f88afce21999621d3d10e73983be7df1
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-29 00:35:48 +0100

    Update demande-e.component.ts

--- FrontEnd/src/app/views/formateur/demande-e/demande-e/demande-e.component.ts
+++ FrontEnd/src/app/views/formateur/demande-e/demande-e/demande-e.component.ts
@@ -8,25 +8,33 @@ import { DataService } from 'src/app/views/services/data.service';
   styleUrls: ['./demande-e.component.scss']
 })
 export class DemandeEComponent implements OnInit {
-  demandes: any; // Définissez correctement le type des demandes
-  helper= new JwtHelperService
-  constructor(private ds: DataService) { } // Assurez-vous d'injecter le service HelperService si nécessaire
+  demandes: any; // Variable to store the list of demandes
+  helper = new JwtHelperService(); // JWT helper instance for decoding tokens
+
+  constructor(private ds: DataService) { }
 
   ngOnInit(): void {
-    const id = this.getId(); // Appel de la méthode pour obtenir l'identifiant du formateur
-    this.loadDemandes(id); // Appel de la méthode pour charger les demandes avec l'ID du formateur
+    const id = this.getId(); // Get the formateur ID from the JWT token
+    this.loadDemandes(id); // Load demandes for the formateur using the ID
   }
 
+  // Method to load demandes based on the formateur ID
   loadDemandes(formateurId: number) {
-    this.ds.getallDemandesByFormateurId(formateurId).subscribe(demandes => {
-      this.demandes = demandes;
-      console.log(demandes)
-    });
+    this.ds.getallDemandesByFormateurId(formateurId).subscribe(
+      demandes => {
+        this.demandes = demandes; // Assign the list of demandes to the component variable
+        console.log(demandes); // Log the demandes data for debugging
+      },
+      error => {
+        console.error('Erreur lors du chargement des demandes :', error); // Log errors if any
+      }
+    );
   }
 
+  // Method to get the formateur ID from the JWT token
   getId(): number {
-    let token: any = localStorage.getItem('token');
-    let decodedtoken: any = this.helper.decodeToken(token);
-    return decodedtoken.id;
+    let token: any = localStorage.getItem('token'); // Retrieve the JWT token from localStorage
+    let decodedtoken: any = this.helper.decodeToken(token); // Decode the token to extract user information
+    return decodedtoken.id; // Return the formateur ID
   }
 }
