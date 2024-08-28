978f19be893ce27182b03abbc590ee535b90ae69
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-28 14:15:38 +0100

    Update login_router.js

--- BackEnd/routers/login_router.js
+++ BackEnd/routers/login_router.js
@@ -1,15 +1,19 @@
-const express=require('express')
-const route=express.Router()
-const login_controller=require('../controllers/login_controller')
+const express = require('express');
+const route = express.Router();
+const login_controller = require('../controllers/login_controller');
 
+//////////////////////////////  Login API  ///////////////////////////////////////////////
 
+// Route to handle user login
+route.post('/login', (req, res) => {
+    // Call the login function from the login_controller with email and password from the request body
+    login_controller.login(req.body.email, req.body.password)
+        .then(token => 
+            res.status(200).json({ token: token }) // Send a success response with the generated token
+        )
+        .catch(err => 
+            res.status(400).json(err) // Send an error response with status 400 if login fails
+        );
+});
 
-route.post('/login',(req,res)=>{
-    login_controller.login(req.body.email,req.body.password)
-    .then(token=>res.status(200).json({token:token}))
-    .catch(err=>res.status(400).json(err))
-    
-    })
-
-
-module.exports=route
+module.exports = route;
