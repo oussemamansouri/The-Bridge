99c6a9df7d6416d85492cff3125b6cbaad062961
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-28 14:14:57 +0100

    Update pack.js

--- BackEnd/models/pack.js
+++ BackEnd/models/pack.js
@@ -1,39 +1,39 @@
-module.exports=(sequelize,DataType)=>{
-    const Pack=sequelize.define("Pack",{
-        titre:{
-            type:DataType.STRING,
-            allowNull:true
-        },
-        description:{
-            type:DataType.STRING,
-            allowNull:true
-        },
-         description1:{
-            type:DataType.STRING,
-            allowNull:true
-        },
-         description2:{
-            type:DataType.STRING,
-            allowNull:true
-        },
-         description3:{
-            type:DataType.STRING,
-            allowNull:true
-        },
-        prix:{
-            type:DataType.FLOAT,
-            allowNull:true
-        },
-        points:{
-            type:DataType.INTEGER,
-            allowNull:true
-        },
-       
+module.exports = (sequelize, DataType) => {
+    // Define the Pack model with its attributes.
+    const Pack = sequelize.define("Pack", {
+      titre: {
+        type: DataType.STRING, // Title of the pack.
+        allowNull: true // Field can be null.
+      },
+      description: {
+        type: DataType.STRING, // General description of the pack.
+        allowNull: true // Field can be null.
+      },
+      description1: {
+        type: DataType.STRING, // Additional description part 1.
+        allowNull: true // Field can be null.
+      },
+      description2: {
+        type: DataType.STRING, // Additional description part 2.
+        allowNull: true // Field can be null.
+      },
+      description3: {
+        type: DataType.STRING, // Additional description part 3.
+        allowNull: true // Field can be null.
+      },
+      prix: {
+        type: DataType.FLOAT, // Price of the pack.
+        allowNull: true // Field can be null.
+      },
+      points: {
+        type: DataType.INTEGER, // Points associated with the pack.
+        allowNull: true // Field can be null.
+      },
     }, {
-        charset: 'utf8mb4',
-        collate: 'utf8mb4_general_ci'
-      })
-
-return Pack
-
-} 
+      charset: 'utf8mb4', // Character set to support a wide range of characters.
+      collate: 'utf8mb4_general_ci' // Collation for case-insensitive comparison.
+    });
+  
+    return Pack; // Return the defined model.
+  };
+  
