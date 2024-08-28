5e452ab9a8e2e1658bf1986e3464e717b50af7e9
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-28 14:15:11 +0100

    Update chat_controller.js

--- BackEnd/controllers/chat_controller.js
+++ BackEnd/controllers/chat_controller.js
@@ -1,21 +1,29 @@
 const db = require('../models');
 
-
-
+// Retrieve all messages for a specific recipient
 exports.getAllMessages = async (recipientId) => {
     try {
-      const messages = await db.ChatMessage.findAll({ where: { recipient_id: recipientId } });
-      return messages;
+        // Fetch all messages where recipient_id matches the given recipientId
+        const messages = await db.ChatMessage.findAll({ where: { recipient_id: recipientId } });
+        return messages;
     } catch (error) {
-      throw new Error('Unable to get all messages');
+        // Throw a descriptive error if message retrieval fails
+        throw new Error('Unable to get all messages');
     }
-  };
-  
-  exports.saveMessage = async (message, senderId, recipientId) => {
+};
+
+// Save a new chat message to the database
+exports.saveMessage = async (message, senderId, recipientId) => {
     try {
-      const savedMessage = await db.ChatMessage.create({ message, sender_id: senderId, recipient_id: recipientId });
-      return savedMessage;
+        // Create a new message record with the provided details
+        const savedMessage = await db.ChatMessage.create({ 
+            message, 
+            sender_id: senderId, 
+            recipient_id: recipientId 
+        });
+        return savedMessage;
     } catch (error) {
-      throw new Error('Unable to save message');
+        // Throw a descriptive error if message saving fails
+        throw new Error('Unable to save message');
     }
-  };
+};
