42e8be5239c99ac71a376e335d813e8fac217821
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-29 16:09:03 +0100

    Update contact.component.ts

--- FrontEnd/src/app/views/front/contact/contact/contact.component.ts
+++ FrontEnd/src/app/views/front/contact/contact/contact.component.ts
@@ -6,39 +6,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent  {
-  formData = {
-    name: '',
-    email: '',
-    subject: '',
-    message: ''
-  };
-  isLoading: boolean = false;
-  isSuccess: boolean = false;
-  errorMessage: string | undefined;
-  nameValue: string = '';
-  emailValue: string = '';
-  subjectValue: string = '';
-  messageValue: string = '';
-  constructor(private http: HttpClient) {}
+export class ContactComponent   {
+  constructor() {}
 
-  // onSubmit(): void {
-  //   this.isLoading = true;
-  //   this.http.post('http://localhost:3000/contact/send-email', this.formData).subscribe(
-  //     (response: any) => {
-  //       this.isLoading = false;
-  //       this.isSuccess = true;
-  //       this.formData = {
-  //         name: '',
-  //         email: '',
-  //         subject: '',
-  //         message: ''
-  //       };
-  //     },
-  //     (error) => {
-  //       this.isLoading = false;
-  //       this.errorMessage = error.message || 'Une erreur s\'est produite lors de l\'envoi de l\'email';
-  //     }
-  //   );
-  // }
 }
