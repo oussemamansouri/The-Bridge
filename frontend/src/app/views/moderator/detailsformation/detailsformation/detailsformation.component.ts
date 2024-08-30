612e9ebd64bf1fa87d6ada4b38ae68a396c2e0d4
oussemamansouri <oussemamansouri619@gmail.com>
2024-08-30 16:46:32 +0100

    Update detailsformation.component.ts

--- frontend/src/app/views/moderator/detailsformation/detailsformation/detailsformation.component.ts
+++ frontend/src/app/views/moderator/detailsformation/detailsformation/detailsformation.component.ts
@@ -10,18 +10,26 @@ import { DataService } from 'src/app/views/services/data.service';
 })
 export class DetailsformationComponent implements OnInit {
 
-  id: string = '';
-  dataobject:any
-  messageerr=''
-  imagepath:any='http://localhost:3000/'
-  constructor(private route:ActivatedRoute,private ds:DataService) {
+  id: string = ''; // Stores the ID of the formation fetched from the route parameters
+  dataobject: any; // Holds the detailed data of the formation
+  messageerr = ''; // Error message in case the formation is not found
+  imagepath: any = 'http://localhost:3000/'; // Base URL for images
+
+  constructor(private route: ActivatedRoute, private ds: DataService) {
+    // Subscribe to route parameters to get the formation's ID
     this.route.params.subscribe((params: Params) => this.id = params['id']);
 
-    this.ds.getoneformation(this.id).subscribe(response=>this.dataobject=response,(err:HttpErrorResponse)=>{
-      console.log(err)
-      this.messageerr="we dont't found this formation in our database"})
-    
-   }
+    // Fetch the formation's details using the ID
+    this.ds.getoneformation(this.id).subscribe(
+      response => this.dataobject = response, // Assign the fetched data to dataobject
+      (err: HttpErrorResponse) => {
+        console.log(err); // Log any errors
+        this.messageerr = "We don't found this formation in our database"; // Display error message if formation is not found
+      }
+    );
+  }
+
   ngOnInit(): void {
+    // Additional initialization tasks can be performed here if needed
   }
 }
